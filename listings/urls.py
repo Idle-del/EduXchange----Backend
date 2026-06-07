c30e9f0010c8fcc308827e33b74915323788fee9
Idle del <magarkiran436@gmail.com>
2026-06-07 15:38:55 +0545

    feat: implement resource CRUD api

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import ResourceListCreate, ResourceDetail
 
 urlpatterns = [
-
+    path('resources/', ResourceListCreate.as_view(), name='resource-list-create'),
+    path('resources/<int:pk>/', ResourceDetail.as_view(), name='resource-detail'),
 ] 
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+# from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+# from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Resource, Category
+from .serializers import ResourceSerializer, CategorySerializer
 
 # Create your views here.
+
+## Function-based View
+
+# @api_view(['GET'])
+# def resource_list(request):
+#     resources = Resource.objects.all()
+    
+#     serializer = ResourceSerializer(resources, many=True)
+    
+#     return Response(serializer.data)
+
+## APIView-based View
+
+# class ResourceList(APIView):
+#     def get(self, request):
+#         resources = Resource.objects.all()
+        
+#         serializer = ResourceSerializer(resources, many=True)
+        
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+## GenericAPIView-based View
+
+# class ResourceList(GenericAPIView):
+#     queryset = Resource.objects.all()
+#     serializer_class = ResourceSerializer
+    
+#     def get(self, request):
+#         resources = self.get_queryset()
+#         serializer = self.get_serializer(resources, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ResourceListCreate(ListCreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    
+class ResourceDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    lookup_field = 'pk'
